8debf26fbb1a8f503e905bc156611639d73e2f49
Grigory <lololohka057@gmail.com>
2023-05-13 15:44:08 +0300

    hot_fix

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,55 +1,55 @@
 from src.item import Item
 
-def test_item1():
+def test_calculate_total_price():
    user1 = Item("Валера", 5000, 10)
    assert user1.calculate_total_price() == 50000
 
-def test_item2():
+def test_apply_discount_1():
    user1 = Item("Валера", 5000, 10)
    user1.apply_discount()
    assert user1.price == 5000
 
-def test_item3():
+def test_apply_discount_2():
    user1 = Item("Валера", 5000, 10)
    user1.apply_discount()
    assert user1.price == 5000
 
-def test_item4():
+def test_apply_discount_3():
    user1 = Item("Валера", 5000, 10)
    Item.pay_rate = 0.5
    user1.apply_discount()
    assert user1.price == 2500
 
-def test_item5():
+def test_name():
    user1 = Item("Валера", 5000, 10)
    assert user1.name == "Валера"
 
-def test_item6():
+def test_price():
    user1 = Item("Валера", 5000, 10)
    assert user1.price == 5000
 
-def test_item7():
+def test_quantity():
    user1 = Item("Валера", 5000, 10)
    assert user1.quantity == 10  
 
-def test_item8():
+def test_name():
    item = Item('Телефон', 10000, 5)
    item.name = 'Смартфон'
    assert item.name == 'Смартфон'
 
-def test_item9():
+def test_instantiate_from_csv():
    Item.instantiate_from_csv()
    assert len(Item.all) == 5
 
-def test_item10():
+def test_string_to_number():
    assert Item.string_to_number('5') == 5
    assert Item.string_to_number('5.0') == 5
    assert Item.string_to_number('5.5') == 5
 
-def test_item11():
+def test_repr():
    item1 = Item("Смартфон", 10000, 20)
    assert repr(item1) =="Item('Смартфон', 10000, 20)"
 
-def test_item12():
+def test_str():
    item1 = Item("Смартфон", 10000, 20)
    assert str(item1) == 'Смартфон'
